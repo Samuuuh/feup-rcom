823087ae3997d455ae11b4400d61e3baf35373fb
Diogo Fernandes <diogosamuelfernandes@outlook.com>
2020-11-05 04:57:32 +0000

    Finalmente acabei isto, só quero que o trabalho corra ffs

--- Guiao3/llfunctions.c
+++ Guiao3/llfunctions.c
@@ -18,6 +18,7 @@ struct termios oldtio,newtio;
 int erro = 0;
 int Ns_Enviado_Write = 0;
 int Ns_Recebido_Read = 0;
+int t = 0;
 
 int llopen(struct applicationLayer *application) {
 
@@ -159,12 +160,12 @@ int llwrite(int fd, unsigned char* buffer, int length) {
     write(fd, &buffer[b], 1);
     b++;
   }
-
+  t++;
   // Receives RR or REJ answer
   int received_NS;
   int received_RR = read_RR(fd, &received_NS);
   printf("received: %d sent %d \n", received_NS, Ns_Enviado_Write);
-  if ((!received_RR) || (received_NS == Ns_Enviado_Write)) { // old if (!received_RR)
+  if ((!received_RR) || (received_NS == Ns_Enviado_Write)) { // old if (!received_RR) add || (t == 500) to simulate error || (t == 500)
     int resent_times_write = 0;
     while (resent_times_write < 3) {
       printf("erro");
@@ -200,11 +201,6 @@ int llwrite(int fd, unsigned char* buffer, int length) {
 }
 
 int llread(int fd, unsigned char* buffer) {
-  if(erro == 400) {
-    printf("ERRO1\n");
-    Ns_Enviado_Write ^= 1;
-  }
-
   erro++;
 
   enum current_state DATA_state = start;
@@ -301,13 +297,18 @@ int llread(int fd, unsigned char* buffer) {
 
   // Se o Ns recebido for igual ao Ns enviado pelo writer, então alterar o valor. Se não, descartar a mensagem. Enviar Ns de qualquer forma
   if(Ns_Recebido_Read == Ns_Enviado_Write) {
-    Ns_Enviado_Write ^= 1;
+    /*
+    if(erro == 400) {
+      printf("ERRO1\n");
+      Ns_Enviado_Write ^= 1;
+    }
+    */
+
+    Ns_Enviado_Write ^= 1; // ou esperado
   }
   else {
     //s descartar mensagem
-    Ns_Enviado_Write ^= 1;
     j = 0;
-    printf("ERRO");
   }
   printf("sent n %d\n", Ns_Enviado_Write);
   write_RR(fd, Ns_Enviado_Write);
@@ -329,7 +330,6 @@ int llread(int fd, unsigned char* buffer) {
   }
   else
     *sizeMessage = 0;
-
   */
 
   return j - 6;
