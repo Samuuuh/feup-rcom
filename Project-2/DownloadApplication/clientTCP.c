9dbe56423d4a634468f5a7e4f0750458e68e4387
Paulinho <up201806505@fe.up.pt>
2020-12-01 19:46:55 +0000

    Done Project 2, but no info read from client socket

--- Project-2/DownloadApplication/clientTCP.c
+++ Project-2/DownloadApplication/clientTCP.c
@@ -13,47 +13,56 @@
 
 #include "utils.h"
 
-#define SERVER_PORT 6000
+#define SERVER_PORT 21
 #define SERVER_ADDR "192.168.28.96"
 
 #define MAX_SIZE 256
 
 int main(int argc, char** argv){
 
-	int	sockfd;
+	int	sockfd, sockfd_client;
 	struct	sockaddr_in server_addr;
+	struct	sockaddr_in server_addr_client;
 	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
 	int	bytes;
 
 	char user[MAX_SIZE];
 	char pass[MAX_SIZE];
 	char host[MAX_SIZE];
-	char url_path[MAX_SIZE];
+	char file_path[MAX_SIZE];
+
+	struct hostent *h;
 
 	if (argc != 2) {
 		fprintf(stderr,"Usage: ./download ftp://[user]:[pass]@[host]/[url-path]\n");
 		exit(1);
 	}
 
-	if (parseArguments(argv[1], user, pass, host, url_path) < 0) {
+	if (parseArguments(argv[1], user, pass, host, file_path) < 0) {
 		fprintf(stderr,"Usage: ./download ftp://[user]:[pass]@[host]/[url-path]\n");
 		exit(2);
 	}
 
-	printf("User: |%s|\nPass: |%s|\nHost: |%s|\nURL path: |%s|\n", user, pass, host, url_path);
+	printf("User: |%s|\nPass: |%s|\nHost: |%s|\nURL path: |%s|\n", user, pass, host, file_path);
 
-	return 0;		// Testar parseArguments - Apagar no futuro
+	if ((h = getIP(host)) == NULL) {
+		fprintf(stderr,"Couldn't get Host IP\n");
+		exit(3);
+	}
+
+	printf("Host name  : %s\n", h->h_name);
+  printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
 	
 	/*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));	/*32 bit Internet address network byte ordered*/
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
     
 	/*open an TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
 		perror("socket()");
-		exit(3);
+		exit(4);
 	}
 
 	/*connect to the server*/
@@ -62,10 +71,65 @@ int main(int argc, char** argv){
 		exit(4);
 	}
 
-  /*send a string to the server*/
-	bytes = write(sockfd, buf, strlen(buf));
-	printf("Bytes escritos %d\n", bytes);
+	char serverResponse[3];
 
-	close(sockfd);
+	readServerResponse(sockfd, serverResponse);
+
+	if (strncmp(serverResponse, "220", 3) != 0) {
+		fprintf(stderr,"Connection lost\n");
+		exit(5);
+	}
+
+	if (login(sockfd, user, pass) < 0) {
+		fprintf(stderr,"Couldn't login\n");
+		exit(6);
+	}
+
+	printf("User logged in\n");
+
+	int port;
+
+	if ((port = activatePassiveMode(sockfd)) < 0) {
+		fprintf(stderr,"Couldn't enter passive mode\n");
+		exit(7);
+	}
+
+	printf("PORT: %d\n", port);
+
+	/*server address handling*/
+	bzero((char*)&server_addr_client,sizeof(server_addr_client));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(port);		/*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	if ((sockfd_client = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+		perror("socket()");
+		exit(4);
+	}
+
+	/*connect to the server*/
+	if(connect(sockfd_client, (struct sockaddr *)&server_addr_client, sizeof(server_addr_client)) < 0) {
+		perror("connect()");
+		exit(4);
+	}
+
+	if (download_file(sockfd, sockfd_client, file_path) < 0) {
+		fprintf(stderr,"Couldn't download file\n");
+		exit(8);
+	}
+
+	printf("File downloaded successfully!\n");
+
+	if (close(sockfd_client) < 0) {
+		perror("Error closing client socket");
+		exit(9);
+	}
+
+	if (close(sockfd)) {
+		perror("Error closing server socket");
+		exit(9);
+	}
+
 	exit(0);
 }
--- Project-2/DownloadApplication/utils.c
+++ Project-2/DownloadApplication/utils.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+#include<arpa/inet.h>
+#include <ctype.h>
+#include <libgen.h>
 
 #include "utils.h"
 
-int parseArguments(char* argument, char* user, char* pass, char* host, char* url_path) {
+#define BUFFER_SIZE 1024
+
+int parseArguments(char* argument, char* user, char* pass, char* host, char* file_path) {
 
   // Parse the initial part of the argument: "ftp://"
   if(strncmp(argument, "ftp://", 6) != 0) {
@@ -41,10 +52,255 @@ int parseArguments(char* argument, char* user, char* pass, char* host, char* url
   k++;
   int l = k;
   while (argument[l] != '\0') {
-    url_path[l - k] = argument[l];
+    file_path[l - k] = argument[l];
     l++;
   }
-  url_path[l - k] = '\0';
+  file_path[l - k] = '\0';
+
+  return 0;
+}
+
+struct hostent* getIP(char *host) {
+	struct hostent *h;
+
+  /*
+  struct hostent {
+      char    *h_name;	Official name of the host. 
+      char    **h_aliases;	A NULL-terminated array of alternate names for the host. 
+      int     h_addrtype;	The type of address being returned; usually AF_INET.
+      int     h_length;	The length of the address in bytes.
+      char    **h_addr_list;	A zero-terminated array of network addresses for the host. 
+      Host addresses are in Network Byte Order. 
+  };
+
+  #define h_addr h_addr_list[0]	The first address in h_addr_list. 
+  */
+
+  if ((h=gethostbyname(host)) == NULL) {  
+    herror("gethostbyname");
+    return NULL;
+  }
+
+  //printf("Host name  : %s\n", h->h_name);
+  //printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+
+  return h;
+}
+
+void readServerResponse(int sockfd, char *response) {
+  enum code_state state = start;
+  char character;
+
+  while(state != code_received) {
+    read(sockfd, &character, 1);
+    //printf("Received %c\n", character);
+
+    switch(state) {
+      case start:
+        if (isdigit(character)) {
+          state = first_digit;
+          response[0] = character;
+        }
+        break;
+      case first_digit:
+        if (isdigit(character)) {
+          state = second_digit;
+          response[1] = character;
+        }
+        else {
+          state = start;
+          memset(response,0,sizeof(response));
+        }
+        break;
+      case second_digit:
+        if (isdigit(character)) {
+          state = third_digit;
+          response[2] = character;
+        }
+        else {
+          state = start;
+          memset(response,0,sizeof(response));
+        }
+        break;
+      case third_digit:
+        if ((character == ' ') || (character == '-')) {
+          state = code_received;
+        }
+        else {
+          state = start;
+          memset(response,0,sizeof(response));
+        }
+        break;
+    }
+  }
+}
+
+int login(int sockfd, char *user, char *pass) {
+  char response[3];
+
+  // Send username
+	write(sockfd, "user ", 5);
+  write(sockfd, user, strlen(user));
+  write(sockfd, "\n", 1);
+
+  readServerResponse(sockfd, response);
+
+  if(strncmp(response, "331", 3) != 0) {
+    fprintf(stderr,"Username not accepted\n");
+		return -1;
+  }
+
+  // Send pass
+	write(sockfd, "pass ", 5);
+  write(sockfd, pass, strlen(pass));
+  write(sockfd, "\n", 1);
+
+  readServerResponse(sockfd, response);
+
+  if(strncmp(response, "230", 3) != 0) {
+    fprintf(stderr,"Password not accepted\n");
+		return -2;
+  }
+
+  return 0;
+}
+
+int activatePassiveMode(int sockfd) {
+  char response[3];
+  char number[3];
+  int i = 0;
+  int pasv_numbers[6];
+  int j = 0;
+
+  write(sockfd, "pasv\n", 5);
+
+  enum pasv_state state = pasv_start;
+  char character;
+
+  while(state != pasv_end) {
+    read(sockfd, &character, 1);
+    //printf("Received %c\n", character);
+
+    switch(state) {
+      case pasv_start:
+        if (character == '(') {
+          state = h1;
+        }
+        break;
+      case h1:
+        if (isdigit(character)) {
+          number[i] = character;
+          i++;
+        }
+        else if (character == ',') {
+          state = h2;
+          pasv_numbers[j] = atoi(number);
+          memset(number,0,sizeof(number));
+          i = 0;
+          j++;
+        }
+        break;
+      case h2:
+        if (isdigit(character)) {
+          number[i] = character;
+          i++;
+        }
+        else if (character == ',') {
+          state = h3;
+          pasv_numbers[j] = atoi(number);
+          memset(number,0,sizeof(number));
+          i = 0;
+          j++;
+        }
+        break;
+      case h3:
+        if (isdigit(character)) {
+          number[i] = character;
+          i++;
+        }
+        else if (character == ',') {
+          state = h4;
+          pasv_numbers[j] = atoi(number);
+          memset(number,0,sizeof(number));
+          i = 0;
+          j++;
+        }
+        break;
+      case h4:
+        if (isdigit(character)) {
+          number[i] = character;
+          i++;
+        }
+        else if (character == ',') {
+          state = p1;
+          pasv_numbers[j] = atoi(number);
+          memset(number,0,sizeof(number));
+          i = 0;
+          j++;
+        }
+        break;
+      case p1:
+        if (isdigit(character)) {
+          number[i] = character;
+          i++;
+        }
+        else if (character == ',') {
+          state = p2;
+          pasv_numbers[j] = atoi(number);
+          memset(number,0,sizeof(number));
+          i = 0;
+          j++;
+        }
+        break;
+      case p2:
+        if (isdigit(character)) {
+          number[i] = character;
+          i++;
+        }
+        else if (character == ')') {
+          state = pasv_end;
+          pasv_numbers[j] = atoi(number);
+        }
+        break;
+    }
+  }
+
+  int port = pasv_numbers[4]*256 + pasv_numbers[5];
+
+  for (int i = 0 ; i < 6 ; i++) {
+    printf("%d\n", pasv_numbers[i]);
+  }
+
+  return port;
+}
+
+int download_file(int sockfd, int sockfd_client, char* file_path) {
+  // Send username
+	write(sockfd, "retr ", 5);
+  write(sockfd, file_path, strlen(file_path));
+  write(sockfd, "\n", 1);
+
+  char* filename;
+
+  filename = basename(file_path);
+
+  FILE *file = fopen(filename, "wb+");
+  
+  char file_part[BUFFER_SIZE];
+  int bytes_read, elems_written;
+  printf("FORA\n");
+
+  while((bytes_read = read(sockfd_client, file_part, BUFFER_SIZE)) > 0) {
+    printf("DENTRO\n");
+    elems_written = fwrite(file_part, bytes_read, 1, file);
+    printf("FILE_PART = %s\n", file_part);
+    if (elems_written != 1) {
+      fprintf(stderr,"Error downloading file\n");
+		  return -1;
+    }
+  }
+
+  fclose(file);
 
   return 0;
 }
